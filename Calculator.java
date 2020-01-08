363fbfbc753a4da409324a541dfb6497849beecb
unknown <mnuyts@LK-CZC722812F.ION.local>
2020-01-08 09:53:47 +0100

    aanpassing

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,6 @@ public class Calculator {
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
 		int resultaat = calc.add(4, 8);
-//		System.out.println(resultaat);
-//		resultaat = calc.add(1452, 546843);
-//		System.out.println(resultaat);
 		System.out.println(calc.divide(5, 2));
 	}
 	
