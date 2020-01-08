916c584c2258e1d995c05b00f67772de16da7cfb
357harold <59644192+357harold@users.noreply.github.com>
2020-01-08 11:00:00 +0100

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,36 @@
 
 public class Calculator {
+
 	
 	public static void main(String[] args) {
+
 		Calculator calc = new Calculator();
+
 		int resultaat = calc.add(4, 8);
-		System.out.println(calc.divide(5, 2));
+	
+	System.out.println(calc.divide(5, 2));
 	}
+
 	
 	public int add(int a, int b) {
-		return a + b;
+	
+	return a + b;
 	}
 	
+
 	public int divideintegers(int a, int b) {
+
 		return a/b;
 	}
 	
+
 	public double divide(double a, double b) {
-		return a/b;
+	
+	return a/b;
 	}
 }
+
+
+// Ja meneer dit trekt dus op niks. Moet dringend verbeterd worden.
+// Groetjes je gezeldige student ;)
+
